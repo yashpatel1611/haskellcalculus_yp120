a8c201960911d3190915a27d77a47e353138cfae
Yash Patel <yp120@ic.ac.uk>
2020-11-05 22:41:48 +0000

    Finished main parts

--- Calculus.hs
+++ Calculus.hs
@@ -29,25 +29,31 @@ instance Vars Double where
   y = 9.2
   z = -1.7
 
+--Added overloads below for mathematical expressions
 instance Num Exp where
-  fromInteger = undefined
-  negate      = undefined
-  (+)         = undefined
-  (*)         = undefined
+  fromInteger x = Val (fromIntegral x)
+  negate x      = UnApp Neg x
+  (+) x y       = BinApp Add x y
+  (*) x y       = BinApp Mul x y
+  -- negate 0      = 0
+  -- (+) 0 x       = Val (fromIntegral x)
+  -- (+) x 0       = Val (fromIntegral x)
 -- Leave the following two undefined...
   signum      = undefined
   abs         = undefined
 
 instance Fractional Exp where
-  fromRational = undefined
-  (/)          = undefined
+  fromRational x = Val (fromRational x)
+  (/) x y        = BinApp Div x y
+  -- (/) 0 x       = 0
+  -- (/) x 1       = x
 -- Leave the following one undefined...
   recip        = undefined
 
 instance Floating Exp where
-  sin     = undefined
-  cos     = undefined
-  log     = undefined
+  sin x     = UnApp Sin x
+  cos x     = UnApp Cos x
+  log x     = UnApp Log x
 -- Leave the following fifteen undefined...
   tan     = undefined
   asin    = undefined
@@ -67,25 +73,114 @@ instance Floating Exp where
 
 ---------------------------------------------------------------------------
 
+--Uses Haskell built-in function lookup to find value for key
 lookUp :: Eq a => a -> [(a, b)] -> b
-lookUp
-  = undefined
+lookUp key table
+  = fromJust (lookup key table)
 
+--Function to convert the long expression into a readable one
+--This is mainly for checking if expressions are correct when
+--testing
 showExp :: Exp -> String
-showExp
-  = undefined
+showExp (BinApp op x1 x2)
+  = ("(" ++ (showExp x1) ++ (getOp op) ++ (showExp x2) ++ ")")
+  where
+    getOp op
+      | op == Add = "+"
+      | op == Mul = "*"
+      | op == Div = "/"
 
+showExp (UnApp op x)
+  = (getOp op) ++ "(" ++ (showExp x) ++ ")"
+  where
+    getOp op
+      | op == Neg = "-"
+      | op == Sin = "sin"
+      | op == Cos = "cos"
+      | op == Log = "log"
+
+showExp (Val a)
+  = show a
+
+showExp (Id c)
+  = c
+
+--Tables below to be used for 'eval' function
+--They map the mathematical expressions to the haskell functions
+unOpMap = [(Neg, (*(-1))), (Sin, sin), (Cos, cos), (Log, log)]
+binOpMap = [(Add, (+)), (Mul, (*)), (Div, (/))]
+
+--eval function will take in a expression and and environment and
+--calculate the result
 eval :: Exp -> Env -> Double
-eval
-  = undefined
+eval (UnApp op x) en
+  = (lookUp op unOpMap) (eval x en)
 
+eval (BinApp op x1 x2) en
+  = (lookUp op binOpMap) (eval x1 en) (eval x2 en)
+
+eval (Val a) _
+  = a
+
+eval (Id c) en
+  = lookUp c en
+
+--diff function will take in an expression and calculate its
+--derivative with respect to a given variable
 diff :: Exp -> String -> Exp
-diff
-  = undefined
+diff (BinApp op x1 x2) v
+  -- = (BinApp Add (BinApp Mul x1 (diff x2 v)) (BinApp Mul (diff x1 v) x2))
+  | op == Add = (diff x1 v) + (diff x2 v)
+  | op == Mul = (x1 * (diff x2 v)) + ((diff x1 v) * x2)
+  | op == Div = (((diff x1 v) * x2) + (negate (x1 * (diff x2 v)))) / (x2 * x2)
+  
 
+diff (Val a) _
+  = Val 0.0
+
+diff (Id c) v
+  | c == v = Val 1.0
+  | otherwise = Val 0.0
+
+diff (UnApp op x) v
+  | op == Neg = negate (diff x v)
+  | op == Sin = (cos x) * (diff x v)
+  | op == Cos = negate ((sin x) * (diff x v))
+  | op == Log = (diff x v) / x
+
+--Redundant code
+{-
+diff :: Exp -> String -> Exp
+diff (BinApp op x1 x2) v
+  -- = (BinApp Add (BinApp Mul x1 (diff x2 v)) (BinApp Mul (diff x1 v) x2))
+  | op == Add = BinApp Add (diff x1 v) (diff x2 v)
+  | op == Mul = BinApp Add (BinApp Mul x1 (diff x2 v)) (BinApp Mul (diff x1 v) x2)
+  | op == Div = BinApp Div (BinApp Add (BinApp Mul (diff x1 v) x2) (UnApp Neg (BinApp Mul x1 (diff x2 v)))) (BinApp Mul x2 x2)
+  
+
+diff (Val a) _
+  = Val 0.0
+
+diff (Id c) v
+  | c == v = Val 1.0
+  | otherwise = Val 0.0
+
+diff (UnApp op x) v
+  | op == Neg = UnApp Neg (diff x v)
+  | op == Sin = BinApp Mul (UnApp Cos x) (diff x v)
+  | op == Cos = UnApp Neg (BinApp Mul (UnApp Sin x) (diff x v))
+  | op == Log = BinApp Div (diff x v) x
+-}
+
+--maclaurin function will perform the maclaurin expansion
+--on a expression by repeated differentiation and substitution
 maclaurin :: Exp -> Double -> Int -> Double
-maclaurin
-  = undefined
+maclaurin ex p n
+  = sum (zipWith3 (\x y z -> (x*z)/y) evs facs xp)
+  where
+    evs = map (`eval` [("x", 0.0)]) (take n (iterate (`diff` "x") ex))
+    facs = 1 : take n (scanl (*) 1 [2..])
+    xp = take n (iterate (*p) 1)
 
 ---------------------------------------------------------------------------
 -- Test cases...
